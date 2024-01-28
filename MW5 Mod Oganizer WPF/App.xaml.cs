a928f673d8156da365df4bdd63af7e089c5c5d2f
Maxim Agemans <Maxim.Agemans@student.ucll.be>
2024-01-28 14:02:25 +0100

    Added DependencyInjection extension into App.xaml.cs

--- MW5 Mod Oganizer WPF/App.xaml.cs
+++ MW5 Mod Oganizer WPF/App.xaml.cs
@@ -1,12 +1,8 @@
-﻿using MW5_Mod_Organizer_WPF.Services;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MW5_Mod_Organizer_WPF.Services;
+using MW5_Mod_Organizer_WPF.Facades;
 using MW5_Mod_Organizer_WPF.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace MW5_Mod_Organizer_WPF
@@ -14,8 +10,26 @@ namespace MW5_Mod_Organizer_WPF
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public sealed partial class App : Application
     {
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Get the <see cref="IServiceProvider"/> instance to resolve application services
+        /// </summary>
+        public IServiceProvider Services { get; private set; }
+
+        public App(bool contentLoaded)
+        {
+            _contentLoaded = contentLoaded;
+            Services = ConfigureServices();
+
+            this.InitializeComponent();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             ModService.GetInstance();
@@ -28,6 +42,28 @@ namespace MW5_Mod_Organizer_WPF
             MainWindow.Show();
             
             base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Configures the services for the application
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Services
+            services.AddTransient<FileHandlerService>();
+            services.AddTransient<JsonHandlerService>();
+            services.AddSingleton<ModService>();
+
+            // Facades
+            services.AddTransient<JsonConverterFacade>();
+
+            // ViewModels
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<ModViewModel>();
+
+            return services.BuildServiceProvider();
         }
     }
 }
