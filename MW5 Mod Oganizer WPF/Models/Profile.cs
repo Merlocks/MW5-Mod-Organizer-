149a4c2b3fad4cf10e701493dc37838e8d30355f
Maxim Agemans <Maxim.Agemans@student.ucll.be>
2024-02-23 00:52:12 +0100

    Added models

---
+++ MW5 Mod Oganizer WPF/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MW5_Mod_Organizer_WPF.Models
+{
+    public class Profile
+    {
+        [JsonPropertyName("mods")]
+        public Dictionary<string, ProfileEntry> Mod;
+
+        public Profile() 
+        { 
+            Mod = new Dictionary<string, ProfileEntry>();
+        }
+    }
+}
---
+++ MW5 Mod Oganizer WPF/Models/ProfileContainer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace MW5_Mod_Organizer_WPF.Models
+{
+    public class ProfileContainer
+    {
+        [JsonPropertyName("profiles")]
+        public Dictionary<string, Profile> Profile;
+
+        public ProfileContainer() 
+        { 
+            Profile = new Dictionary<string, Profile>();
+        }
+    }
+}
