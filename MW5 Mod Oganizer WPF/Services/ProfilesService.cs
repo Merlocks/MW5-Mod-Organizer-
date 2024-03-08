5306120c2f76c5e1acd93da48f93a90f5b760bda
Maxim Agemans <Maxim.Agemans@student.ucll.be>
2024-03-08 23:01:03 +0100

    Added ExportProfile function

--- MW5 Mod Oganizer WPF/Services/ProfilesService.cs
+++ MW5 Mod Oganizer WPF/Services/ProfilesService.cs
@@ -3,6 +3,7 @@ using System;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MW5_Mod_Organizer_WPF.Services
 {
@@ -79,6 +80,34 @@ namespace MW5_Mod_Organizer_WPF.Services
             catch (Exception e)
             {
                 Console.WriteLine($"-- ProfilesService.SaveProfiles -- {e.Message}");
+            }
+        }
+
+        public void ExportProfile(Profile profile)
+        {
+            try
+            {
+                var dialog = new SaveFileDialog
+                {
+                    Title = $"Save {profile.Name}.json",
+                    FileName = $"{profile.Name}",
+                    Filter = "Json file (*.json)|*.json",
+                    FilterIndex = 0,
+                    DefaultExt = "exe",
+                    RestoreDirectory = true
+                };
+                DialogResult result = dialog.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    JsonSerializerOptions options = new JsonSerializerOptions() { WriteIndented = true };
+                    string jsonString = JsonSerializer.Serialize<Profile>(profile, options);
+
+                    File.WriteAllText($"{dialog.FileName}", jsonString);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"-- ProfilesService.ExportProfile -- {e.Message}");
             }
         }
     }
