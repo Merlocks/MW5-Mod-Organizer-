7282b1f55a70f9fabbd5a6ee06affed783446024
Maxim Agemans <Maxim.Agemans@student.ucll.be>
2024-03-14 11:27:46 +0100

    Removed beta title. Increased minwidth of author and version column. Changed default game version to 1.1.354

--- MW5 Mod Oganizer WPF/Properties/Settings.Designer.cs
+++ MW5 Mod Oganizer WPF/Properties/Settings.Designer.cs
@@ -37,7 +37,7 @@ namespace MW5_Mod_Organizer_WPF.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.1.351")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.1.354")]
         public string GameVersion {
             get {
                 return ((string)(this["GameVersion"]));
