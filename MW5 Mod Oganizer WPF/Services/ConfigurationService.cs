767a5b0c246f38376d9dea7f41fc19afac96d05c
Maxim Agemans <Maxim.Agemans@student.ucll.be>
2024-07-14 14:55:21 +0200

    Changed setter of config property to private.

--- MW5 Mod Oganizer WPF/Services/ConfigurationService.cs
+++ MW5 Mod Oganizer WPF/Services/ConfigurationService.cs
@@ -5,13 +5,13 @@ namespace MW5_Mod_Organizer_WPF.Services
 {
     public sealed class ConfigurationService
     {
-        public IConfiguration config { get; set; }
+        public IConfiguration config { get; private set; }
 
         public ConfigurationService() 
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true); 
 
             this.config = builder.Build();
 
